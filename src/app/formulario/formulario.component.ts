f0d983043e10843198fa67f7b130c33dc89b9a25
rafa0206 <rafaelfernandes0206@gmail.com>
2021-04-20 20:45:42 -0300

    atualizacao

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,24 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Pessoa } from '../pessoa';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
-export class FormularioComponent implements OnInit {
 
-  constructor() { }
+export class FormularioComponent  {
 
-  ngOnInit(): void {
+  pessoa: Pessoa = new Pessoa();
+
+  profissoes=['Engenheiro', 'Professor', 'Quimico', 'Zoólogo'];
+
+  salvar(pessoaForm: NgForm) {
+    this.pessoa.nome = pessoaForm.value.nome;
+    this.pessoa.fone = pessoaForm.value.fone;
+    this.pessoa.email = pessoaForm.value.email;
+    this.pessoa.profissao = pessoaForm.value.profissao;
+    console.log(this.pessoa);
+    console.log(pessoaForm);
   }
-
-  salvar(pessoaForm) {
-    const nome = pessoaForm.value.nome;
-    const fone = pessoaForm.value.fone;
-    const email = pessoaForm.value.email;
-    const profissao = pessoaForm.value.profissao;
-    console.log(`Nome: ${nome}, Fone: ${fone}, Email: ${email}, Profissão: ${profissao}`)
-    }
-
 }
+
